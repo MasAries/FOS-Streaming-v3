77101f5a3d948ac12704d5d4052bb51e93363d00
abtammuz <abtammuz@hotmail.com>
2018-02-08 19:13:39 -0600

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -8,6 +8,6 @@ cd /tmp
 wget https://github.com/abtammuz/FOS-Streaming-v3/raw/master/install.php.1
 mv install.php.1 install.php
 php install.php
-wget https://github.com/andykimpe/FOS-Streaming-v3/raw/master/db_install.sh
+wget https://github.com/abtammuz/FOS-Streaming-v3/raw/master/db_install.sh
 chmod 755 db_install.sh
 ./db_install.sh
